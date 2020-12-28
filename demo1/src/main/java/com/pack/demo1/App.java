0ebd182b65f1a0243fd9f73522edee8c164ad88d
Anitha <Damodar@DESKTOP-J4ICCHK>
2020-12-28 19:11:06 +0530

    sample mvc

---
+++ demo1/src/main/java/com/pack/demo1/App.java
@@ -0,0 +1,13 @@
+package com.pack.demo1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
